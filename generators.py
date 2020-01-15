d536fed5c7f871019f5542898fa58df4bec4a8ad
aqim <folly77folly@gmail.com>
2020-01-15 10:19:32 +0100

    keyword args

--- generators.py
+++ generators.py
@@ -1,4 +1,23 @@
 # Document at least 3 use cases of generators
-u=[10, 222, 31, 42, 79]
-a=iter(u)
-print(a)
+
+# a generator to get even numbers btw
+def get_even(num):
+    for number in range(start, stop):
+        if number % 2 == 0:
+            yield number
+print(next(get_even(6)))
+
+
+#get the square root of a list
+lst = [1, 3, 4]
+result = (numbs ** 2 for numbs in lst)
+print(next(result))
+
+# to get all the names that start with  letter a 
+lst2 = ['tunde', 'solape', 'Anthony', 'ake']
+def gen_first_letter(lst2):
+    for name in lst2:
+        if name.lower()[0:1] == 'a':
+            yield name
+
+print(next(gen_first_letter(lst2)))
--- keyword_arguments.py
+++ keyword_arguments.py
@@ -1,3 +1,11 @@
 # Document at least 3 use cases of the * and ** operators
 
-# * in python is 
+# * in python is asterisks operator 
+# case 1 to add numbers i dont now the numbers
+def adder(*num):
+    sum = 0
+    
+    for n in num:
+        sum = sum + n
+    print("Sum:",sum)
+
